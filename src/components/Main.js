28fba7e488419760d86bd3816a589bbdee08bf7b
bqgama <bqgama@gmail.com>
2022-09-27 19:44:51 -0300

    added react icons; edited main.css

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,9 +2,19 @@ import React, { Component } from "react";
 
 import './Main.css'
 
+//Form
+import { FaPlus } from 'react-icons/fa'
+
+//Tarefas
+import { FaEdit, FaWindowClose } from 'react-icons/fa'
 export default class Main extends Component {
   state = {
-    novaTarefa: ''
+    novaTarefa: '',
+    tarefas: [
+      "Fazer café",
+      "Beber água",
+      "Estudar"
+    ]
   };
 
   handleChange = (e) => {
@@ -14,15 +24,33 @@ export default class Main extends Component {
   }
 
   render() {
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas} = this.state;
     return (
       <div className="main">
         <h1>Lista de Tarefas</h1>
 
-        <form action="#">
-          <input onChange={this.handleChange} type="text" />
-          <button type="submit">Enviar</button>
+        <form action="#" className="form">
+          <input
+            onChange={this.handleChange}
+            type="text"
+            value={ novaTarefa }  
+          />
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
+
+        <ul className="tarefas">
+          {tarefas.map(tarefa => (
+            <li key={ tarefa }>
+              { tarefa }
+              <div>
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     )
   }
